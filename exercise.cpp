269f32287efe2fa69e65d36bcc1db55b221afe06
enom1S <GRRSMN01E01I754J@studium.unict.it>
2022-05-08 16:39:52 +0200

    refactoring completed

--- exercise.cpp
+++ exercise.cpp
@@ -8,34 +8,52 @@
 
 #include <iostream>
 
- using namespace std;
+using namespace std;
 
-int main()
-{
+int inserisciDimensione(int dim){
+    cin>>dim;
+    return dim;
+}
 
+void inserisciElementi(int *a, int dim){
+    int x;
+    for(int i=0; i<dim; i++){
+        cin>>x;
+        a[i]=x;
+    }
+}
+
+ void swap(int &a, int &b){
+    int tmp;
+    tmp=a;
+    a=b;
+    b=tmp;
+}
+
+void bubbleSort(int *a, int dim){
+    for(int i=0; i<dim-1; i++){
+        for(int j=0; j<dim-i-1; j++){
+            if(a[j]>a[j+1])swap(a[j],a[j+1]);
+        }
+    }
+}
+
+void visualizzaArray(int *a, int dim){
+    for(int i=0; i<dim; i++){
+        cout<<a[i]<<" ";
+    }
+}
+
+int main(){
     cout<<"Inserisci dimensione: ";
-    int N;
-    cin>>N;
-    int a[N], i, j, temp;
-
-      cout << "Insert array elements:\n";
-      for (i = 0; i < N; i++)
-      {
-        cin >> a[i];
-      }
-        for (i = 0; i < N; i++)
-        {
-          if (a[i] > a[i + 1])
-          {
-            temp = a[i];
-            a[i] = a[i + 1] ;
-            a[i + 1] = temp;
-          }
-      }
-
-      cout << "Sorted array by bubble sort:" << endl;
-      for (i = 0; i < N; i++)
-      {
-        cout << a[i];
-      }
+    int dim=inserisciDimensione(dim);
+    int *a=new int [dim];
+    cout<<"Inserisci gli elementi: ";
+    inserisciElementi(a,dim);
+    visualizzaArray(a, dim);
+    cout<<"\n";
+    bubbleSort(a, dim);
+    cout<<"\n";
+    visualizzaArray(a, dim);
+    return 0;
 }
