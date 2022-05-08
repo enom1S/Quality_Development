b75cb57bf7b019b9b0dd07d7d9e84ee341603227
enom1S <GRRSMN01E01I754J@studium.unict.it>
2022-05-08 16:19:30 +0200

    made N dynamic

--- exercise.cpp
+++ exercise.cpp
@@ -7,36 +7,39 @@
 */
 
 #include <iostream>
-#define N 10
 
  using namespace std;
 
 int main()
 {
-  int a[N], i, j, temp;
 
-  cout << "Insert array elements:\n";
-  for (i = 0; i < N; i++)
-  {
-    cin >> a[i];
-  }
+    cout<<"Inserisci dimensione: ";
+    int N;
+    cin>>N;
+    int a[N], i, j, temp;
 
-  for (j = 0; j < N; j++)
-  {
-    for (i = 0; i < N; i++)
-    {
-      if (a[i] > a[i + 1])
+      cout << "Insert array elements:\n";
+      for (i = 0; i < N; i++)
       {
-        temp = a[i];
-        a[i] = a[i + 1] ;
-        a[i + 1] = temp;
+        cin >> a[i];
       }
-    }
-  }
 
-  cout << "Sorted array by bubble sort:" << endl;
-  for (i = 0; i < N; i++)
-  {
-    cout << a[i];
-  }
+      for (j = 0; j < N; j++)
+      {
+        for (i = 0; i < N; i++)
+        {
+          if (a[i] > a[i + 1])
+          {
+            temp = a[i];
+            a[i] = a[i + 1] ;
+            a[i + 1] = temp;
+          }
+        }
+      }
+
+      cout << "Sorted array by bubble sort:" << endl;
+      for (i = 0; i < N; i++)
+      {
+        cout << a[i];
+      }
 }
